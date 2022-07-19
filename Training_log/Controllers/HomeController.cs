a0fc08f6da8335542a83b11d7bfad88c0b50eb2e
William_Twomey <irowt@mail.com>
2022-07-19 22:21:32 +0100

    Added previous lifts to be displayed on the home screen also worked on some styling.

--- Training_log/Controllers/HomeController.cs
+++ Training_log/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Training_log.Models;
+using Training_log.ViewModels;
 
 namespace Training_log.Controllers
 {
@@ -10,21 +12,66 @@ namespace Training_log.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
+            using(Weight_TrainingDBEntitiesCTX db = new Weight_TrainingDBEntitiesCTX())
+            {
+                var squatLift = db.Squats;
+                var benchLift = db.Benches;
+                var deadLift = db.Deadlifts;
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+                foreach(var squat in squatLift)
+                {
+                    var squatVM = new LiftsVM();
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+                    squatVM.Date = squat.DateOfLift;
+                    var squatDate = squat.DateOfLift.ToString("dd/MM/yyyy");
 
+
+                    squatVM.WeightLifted = squat.WeightKG;
+                    squatVM.Reps = squat.Repetitions;
+
+                    ViewBag.SquatLiftDate = squatDate;
+                    ViewBag.SquatLiftWeight = squatVM.WeightLifted;
+                    ViewBag.SquatLiftReps = squatVM.Reps;
+                    
+                }
+
+                foreach (var bench in benchLift)
+                {
+                    var benchVM = new LiftsVM();
+
+                    benchVM.Date = bench.DateOfLift;
+                    benchVM.WeightLifted = bench.WeightKG;
+                    benchVM.Reps = bench.Repetitions;
+                    
+                    //Converting to date format dd/mm/yyyy to get rid of timestamp
+                    var benchDate = bench.DateOfLift.ToString("dd/MM/yyyy");
+
+                    ViewBag.BenchLiftDate = benchDate;
+                    ViewBag.BenchLiftWeight = benchVM.WeightLifted;
+                    ViewBag.BenchLiftReps = benchVM.Reps;
+
+                }
+
+                foreach (var dead in deadLift)
+                {
+                    var deadVM = new LiftsVM();
+
+                    deadVM.Date = dead.DateOfLift;
+                    //Converting to date format dd/mm/yyyy to get rid of timestamp
+                    var deadDate = dead.DateOfLift.ToString("dd/MM/yyyy");
+                    
+                    deadVM.WeightLifted = dead.WeightKG;
+                    deadVM.Reps = dead.Repetitions;
+
+                    ViewBag.DeadLiftDate = deadDate;
+                    ViewBag.DeadLiftWeight = deadVM.WeightLifted;
+                    ViewBag.DeadLiftReps = deadVM.Reps;
+
+                }
+            }
             return View();
         }
     }
 }
+                    
