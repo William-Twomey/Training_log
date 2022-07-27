892ccc78ddbd98502872aefcf99e29cd69a4115c
William_Twomey <irowt@mail.com>
2022-07-27 19:47:27 +0100

    Added in libaries to use later. Added in squat pound view and controller.

---
+++ Training_log/Controllers/SquatPoundsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Training_log.Models;
+using Training_log.ViewModels;
+
+namespace Training_log.Controllers
+{
+    public class SquatPoundsController : Controller
+    {
+
+        // GET: SquatPounds
+        public ActionResult Index()
+        {
+            using (Weight_TrainingDBEntitiesCTX db = new Weight_TrainingDBEntitiesCTX())
+            {
+                var liftSquat = db.Squats;
+
+                var liftView = new List<LiftsVM>();
+
+                foreach (var lift in liftSquat)
+                {
+                    var squatVM = new LiftsVM();
+
+                    squatVM.Id = lift.Id;
+                    squatVM.Date = lift.DateOfLift;
+                    squatVM.WeightLifted = lift.WeightKG * 2.205;
+                    squatVM.Reps = lift.Repetitions;
+
+                    liftView.Add(squatVM);
+                }
+                return View(liftView);
+
+            }
+        }
+    }
+}
